c78ec0cfc5abc396bdcb89844f623fa8e08367d0
hsiangyichen <hsiangyi1025@gmail.com>
2025-01-13 20:27:20 -0800

    feat: Set up project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.scss";
+import Header from "./components/Header/Header.jsx";
+import Main from "./components/Main/Main.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
